f5b1cf2f2c48163c9f0ff2015f7af3fb2f356837
ohseungmin <ohmin2886@gmail.com>
2024-02-02 14:28:24 +0900

    Link를 사용하기 위해서 react-router-dom을 설치하고 Link를 사용해준다.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+    <Link to="https://reactrouter.com/en/main/start/overview">이동</Link>
+    </div>
+  );
+}
+
+export default App;
